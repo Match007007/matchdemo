a0bc7367c481409a7aa45680633a976d8c29ffb9
Match007007 <match007007@gmail.com>
2022-02-22 22:45:21 +0800

    version 3

--- script.js
+++ script.js
@@ -1,65 +1,110 @@
 'use strict';
+
 /*
-console.log(document.querySelector('.message').textContent); // get the element information by selecting the name of the class and use the methods to do that ( method means function ,  xxxxx.xxx.xxx, those dots are called methods)
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-//DOM  Document Object Model is a a pathway for the javascript to get access to the html content, attributes and even css elements and changed them )
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
 
-//document here is the object and queryselector is the method
-
-//DOM !== javascript !!!!!!!!!!
-
-// DOM is not part of javascropt , we use the API, Application Programming Interface (Libraries written in javascript), we are just using the webapi to do the DOM
-
-document.querySelector('.message').textContent = 'Correct Number';
-
-document.querySelector('.number').textContent = '13';
-
-document.querySelector('.score').textContent = '10';
-document.querySelector('.guess').value = '23';
-
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
 */
 
-let secretNumber = Math.trunc(Math.random() * 20 + 1);
-document.querySelector('.number').textContent = '?';
-let Score = 20;
-let Highscore = 0;
-document.querySelector('.again').addEventListener('click', function () {
-  secretNumber = Math.trunc(Math.random() * 20 + 1);
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.highscore').textContent = Highscore;
-  Score = 20;
-  document.querySelector('.score').textContent = Score;
-  document.querySelector('.message').textContent = '👶🏻冇禁易比你估中既👶🏻';
-});
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  1;
   console.log(guess, typeof guess);
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('body').style.backgroundColor = '#000000';
 
+  // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent =
-      '🧐醒呀喂, 填個數學字,大佬!!!!';
+    // document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');
+
+    // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '估中左,抵錫💋';
+    // document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
+
     document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
 
-    document.querySelector('.number').style.width = '20rem';
-
-    if (Score > Highscore) {
-      Highscore = Score;
-      document.querySelector('.highscore').textContent = Highscore;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '😅太大啦 ';
-    Score--;
-    document.querySelector('.score').textContent = Score;
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '😅太細啦 ';
-    Score--;
-    document.querySelector('.score').textContent = Score;
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = '💥 You lost the game!';
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
   }
+
+  //   // When guess is too high
+  // } else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📈 Too high!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+
+  //   // When guess is too low
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📉 Too low!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
